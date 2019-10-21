628150da1471c6aa0228df433215d1b76790d158
Annie <46758400+stunningspacekid@users.noreply.github.com>
2019-10-21 19:42:27 +0300

    Update opt.py

--- opt.py
+++ opt.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+
+
 from collections import namedtuple
+import numpy as np
+
 
 
 Result = namedtuple('Result', ('nfev', 'cost', 'gradnorm', 'x'))
@@ -19,3 +23,56 @@ gradnorm : float
 x : 1-d array
     Финальное значение вектора, минимизирующего функцию потерь
 """
+
+
+def lm(y, f, j, x0, lmbd0=1e-2, nu=2, tol=1e-4):
+    
+    nfev = 0
+    cost = []
+    x = x0.copy()
+    lmbd = lmbd0
+    residual = y - f(*x)
+    jac = j(*x)
+    
+    def delta(x, jac, lmbd):
+        grad = jac.T @ (y - f(*x))
+        return np.linalg.solve(jac.T @ jac + lmbd * np.identity(np.shape(grad)[0]), grad)
+          
+    def F(x):
+        return 0.5 * (y - f(*x)) @ ( y - f(*x))
+    
+    while np.linalg.norm(delta(x, jac, lmbd)) > tol:   
+        cost.append(F(x))     
+        nfev += 1
+        gradnorm = np.linalg.norm(jac.T @ (y - f(*x))) 
+        F1 = F(x + delta(x, j(*x), lmbd))
+        F2 = F(x + delta(x, j(*x), lmbd/nu))
+        if F2 <= F(x):
+            lmbd/=nu 
+        else:
+            if F1 <=F (x):
+                lmbd = lmbd
+            else:
+                while F1>=F(x) and np.linalg.norm(delta(x, jac, lmbd))>tol:
+                    lmbd*=nu
+                    
+        x+=delta(x, j(*x), lmbd)
+   
+    return Result(nfev, cost, gradnorm, x)
+
+def gauss_newton(y, f, j, x0, k=1, tol=1e-4):
+    nfev = 0 
+    cost = [] 
+    x=x0.copy()
+    delta_x = np.full_like(x0, 2*tol) 
+    while np.linalg.norm(delta_x) > tol:
+        nfev += 1
+        residual = y - f(*x) 
+        cost.append( 0.5 * residual @ residual) 
+        jac = j(*x) 
+        grad = jac.T @ residual 
+        gradnorm = np.linalg.norm(grad) 
+        delta_x = np.linalg.solve(jac.T @ jac, grad)
+        x+= k * delta_x  
+    cost = np.array(cost)
+    return Result(nfev, cost, gradnorm, x)
